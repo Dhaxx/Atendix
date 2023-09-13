60764d532930c62379da4fca8bf3d476a11cfe39
Dhaxx <kaiopablo44@gmail.com>
2023-09-13 16:59:37 -0300

    Estilizando Mensagem de Erro do Login

--- login.py
+++ login.py
@@ -14,18 +14,18 @@ def login():
     usuario = request.form.get('usuario')
     senha = request.form.get('senha')
 
-    conn = sqlite3.connect("F:\Kaio\PROGRAMAÇÃO\Atendix\database\saturndb")
+    conn = sqlite3.connect("./database/saturndb")
     cursor = conn.cursor()
     salt = cursor.execute("SELECT salt FROM USUARIOS WHERE USUARIO = ?", (usuario,)).fetchone()
 
     if salt:
         hash = cript.valida_hash(senha=senha, salt=salt[0])
-        login = cursor.execute("SELECT * FROM USUARIOS WHERE USUARIO = ? AND SENHA = ? AND HASH = ? AND STATUS = 'A'", (usuario, senha, hash)).fetchone()
+        login = cursor.execute("SELECT * FROM USUARIOS WHERE USUARIO = ? AND HASH = ? AND STATUS = 'A'", (usuario, hash)).fetchone()
         
         if login:
             data_atual = datetime.datetime.now()
             cursor.execute("update usuarios set ult_acesso = ? where usuario = ?", (data_atual, login[1]))
             conn.close()
             return redirect(url_for("main.main"))
-    else:
-        return render_template("login.html", error="Usuário ou senha inválida")
+    if salt is None or login is None:
+            return render_template("login.html", error="Usuário ou senha inválida")
