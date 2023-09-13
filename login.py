047b97dc6257099b02416dcdaf26ffd72858603f
Dhaxx <kaiopablo44@gmail.com>
2023-09-13 11:18:50 -0300

    Adicionando Mensagem de Erro ao Login

--- login.py
+++ login.py
@@ -14,17 +14,18 @@ def login():
     usuario = request.form.get('usuario')
     senha = request.form.get('senha')
 
-    conn = sqlite3.connect("D:\Programação\Atendix\database\saturndb")
+    conn = sqlite3.connect("F:\Kaio\PROGRAMAÇÃO\Atendix\database\saturndb")
     cursor = conn.cursor()
-    salt = cursor.execute("SELECT salt FROM USUARIOS WHERE USUARIO = ?", (usuario,)).fetchone()[0]
-    data_atual = datetime.datetime.now()
-    hash = cript.valida_hash(senha=senha, salt=salt)
+    salt = cursor.execute("SELECT salt FROM USUARIOS WHERE USUARIO = ?", (usuario,)).fetchone()
 
-    login = cursor.execute("SELECT * FROM USUARIOS WHERE USUARIO = ? AND SENHA = ? AND HASH = ? AND STATUS = 'A'", (usuario, senha, hash)).fetchone()
-
-    if login:
-        cursor.execute("update usuarios set ult_acesso = ? where usuario = ?", (data_atual, login[1]))
-        conn.close()
-        return redirect(url_for("main.main"))
+    if salt:
+        hash = cript.valida_hash(senha=senha, salt=salt[0])
+        login = cursor.execute("SELECT * FROM USUARIOS WHERE USUARIO = ? AND SENHA = ? AND HASH = ? AND STATUS = 'A'", (usuario, senha, hash)).fetchone()
+        
+        if login:
+            data_atual = datetime.datetime.now()
+            cursor.execute("update usuarios set ult_acesso = ? where usuario = ?", (data_atual, login[1]))
+            conn.close()
+            return redirect(url_for("main.main"))
     else:
         return render_template("login.html", error="Usuário ou senha inválida")
