835b6f7bfdfe950fa170a21aa672ee47ddb32fb5
Dhaxx <kaiopablo44@gmail.com>
2023-09-19 01:23:58 -0300

    Adicionando Validação ao Recarregar Página de Atendimento

--- atendimentos.py
+++ atendimentos.py
@@ -22,6 +22,19 @@ def registra_atendimento():
         cursor = conn.cursor()
         data_registro = datetime.datetime.now()
 
+        ultimo_atendimento = cursor.execute(f"select * from atendimentos where usuario = '{usuario}' order by atendimento_id desc limit 1").fetchone()
+
+        if ultimo_atendimento:
+            if (
+                ultimo_atendimento[1] == cliente and
+                ultimo_atendimento[2] == entidade and
+                ultimo_atendimento[3] == municipio and
+                ultimo_atendimento[4] == observacoes
+            ):
+                conn.close()
+                mensagem = "O novo atendimento é idêntico ao último. Não foi registrado."
+                return render_template("atendimento.html", msg_erro=mensagem)
+
         try:
             cursor.execute("Insert into Atendimentos (cliente, entidade, municipio, obs, dt_atendimento, usuario) values (?,?,?,?,?,?)", 
                         (cliente, entidade, municipio, observacoes, data_registro, usuario))
